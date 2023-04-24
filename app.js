e0990c9b4e9450fe4b871d5b40503b0cbac3a675
Daniel Clough <115506656+daniellee862@users.noreply.github.com>
2023-04-24 19:22:45 +0100

    add cors middleware

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 //Imports
 const express = require("express");
+const cors = require("cors");
 const examRoute = require("./routes/exam");
 const { handleServerErrors } = require("./errors");
 
@@ -8,6 +9,9 @@ const app = express();
 
 //add middleware function to parse json
 app.use(express.json());
+
+//middleware to allow CORS (Cross-Origin Resource Sharing)
+app.use(cors());
 
 //api routes
 app.use("/api/exams", examRoute);
