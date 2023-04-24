21004ebfd1f0672c3c5fb2ec4278c32e4ee147bd
Daniel Clough <115506656+daniellee862@users.noreply.github.com>
2023-04-24 12:24:55 +0100

    implement query params in controller and filter in model

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,8 +1,15 @@
 const { readAllExams } = require("../models/models");
 
 const fetchAllExams = async (req, res, next) => {
+  const { filter_by, filter_term, start_date, end_date } = req.query;
+
   try {
-    const allExamsArray = await readAllExams();
+    const allExamsArray = await readAllExams(
+      filter_by,
+      filter_term,
+      start_date,
+      end_date
+    );
     res.status(200).send(allExamsArray);
   } catch (error) {
     console.error(error);
--- models/models.js
+++ models/models.js
@@ -1,10 +1,43 @@
 const fs = require("fs/promises");
 
-const readAllExams = async () => {
+const readAllExams = async (filterBy, filterTerm, startDate, endDate) => {
   try {
+    //read local file and parse json
     const allExams = await fs.readFile("./data/exams.json", "UTF8");
     const allExamsArray = JSON.parse(allExams);
-    return allExamsArray;
+
+    //Filter the exams based on query params
+    if (filterBy) {
+      // Check if filter_by is a valid property name
+      if (!allExamsArray[0].hasOwnProperty(filterBy)) {
+        throw new Error(`Invalid filter_by parameter: ${filterBy}`);
+      }
+
+      const filteredExams = allExamsArray.filter((exam) => {
+        if (filterBy === "Date") {
+          // Parse the exam date string into a Date object
+          const examDate = new Date(exam.Date);
+          // Parse the filter start date string into a Date object
+          const startDateObject = new Date(startDate);
+          // Parse the filter end date string into a Date object
+          const endDateObject = new Date(endDate);
+          // Check if the exam date falls within the filter range
+          return (
+            examDate.getTime() >= startDateObject.getTime() &&
+            examDate.getTime() <= endDateObject.getTime()
+          );
+        } else {
+          // Filter by name or location
+          return exam[filterBy]
+            .toLowerCase()
+            .includes(filterTerm.toLowerCase());
+        }
+      });
+
+      return filteredExams;
+    } else {
+      return allExamsArray;
+    }
   } catch (error) {
     console.error(error);
     next(error);
