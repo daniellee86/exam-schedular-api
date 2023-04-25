3cfa1b0f1d5fe5bb86c5b0459dd9e310178b047b
Daniel Clough <115506656+daniellee862@users.noreply.github.com>
2023-04-25 12:47:16 +0100

    refactor api

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,15 +1,14 @@
 const { readAllExams } = require("../models/models");
 
 const fetchAllExams = async (req, res, next) => {
-  const { filter_by, filter_term, start_date, end_date } = req.query;
+  // const { filter_by, filter_term, start_date, end_date } = req.query;
 
   try {
-    const allExamsArray = await readAllExams(
-      filter_by,
-      filter_term,
-      start_date,
-      end_date
-    );
+    const allExamsArray = await readAllExams();
+    // filter_by,
+    // filter_term,
+    // start_date,
+    // end_date
     res.status(200).send(allExamsArray);
   } catch (error) {
     console.error(error);
--- models/models.js
+++ models/models.js
@@ -1,43 +1,16 @@
 const fs = require("fs/promises");
-const {
-  filterExamsByDate,
-  filterExamsByNameOrLocation,
-} = require("../utils/filters");
+// const {
+//   filterExamsByDate,
+//   filterExamsByNameOrLocation,
+// } = require("../utils/filters");
 
-const readAllExams = async (filterBy, filterTerm, startDate, endDate, next) => {
+const readAllExams = async (next) => {
+  // filterBy, filterTerm, startDate, endDate,
   try {
     //read local file and parse json
     const allExams = await fs.readFile("./data/exams.json", "UTF8");
     const allExamsArray = JSON.parse(allExams);
-
-    //Filter the exams based on query params
-    if (filterBy) {
-      // Check if filterBy is a valid property name
-      if (!allExamsArray[0].hasOwnProperty(filterBy)) {
-        throw new Error(`Invalid filter_by parameter: ${filterBy}`);
-      }
-
-      if (filterBy === "Date") {
-        const filteredByDate = filterExamsByDate(
-          allExamsArray,
-          startDate,
-          endDate
-        );
-        //Return exams filtered and sorted by Date
-        return filteredByDate.sort((a, b) => a.Date - b.Date);
-      } else {
-        const filteredByNameOrLocation = filterExamsByNameOrLocation(
-          allExamsArray,
-          filterBy,
-          filterTerm
-        );
-        //Return exams filtered by name or location and sorted by date
-        return filteredByNameOrLocation.sort((a, b) => a.Date - b.Date);
-      }
-    } else {
-      //Return all exams sorted by date
-      return allExamsArray.sort((a, b) => a.Date - b.Date);
-    }
+    return allExamsArray.sort((a, b) => a.Date - b.Date);
   } catch (error) {
     console.error(error);
     next(error);
@@ -45,3 +18,36 @@ const readAllExams = async (filterBy, filterTerm, startDate, endDate, next) => {
 };
 
 module.exports = { readAllExams };
+
+//DONT NEED THIS SERVER SIDE LOGIC - FILTERING WILL HAPPEN CLIENT SIDE FOR THIS PROJECT
+
+/*   
+   //Filter the exams based on query params
+   if (filterBy) {
+     // Check if filterBy is a valid property name
+     if (!allExamsArray[0].hasOwnProperty(filterBy)) {
+       throw new Error(`Invalid filter_by parameter: ${filterBy}`);
+     }
+
+     if (filterBy === "Date") {
+       const filteredByDate = filterExamsByDate(
+         allExamsArray,
+         startDate,
+         endDate
+       );
+       //Return exams filtered and sorted by Date
+       return filteredByDate.sort((a, b) => a.Date - b.Date);
+     } else {
+       const filteredByNameOrLocation = filterExamsByNameOrLocation(
+         allExamsArray,
+         filterBy,
+         filterTerm
+       );
+       //Return exams filtered by name or location and sorted by date
+       return filteredByNameOrLocation.sort((a, b) => a.Date - b.Date);
+     }
+   } else {
+     //Return all exams sorted by date
+     return allExamsArray.sort((a, b) => a.Date - b.Date);
+    
+   } */
